72e22938a38e2a5a5a9f3392d63842dca276b465
Luhter1 <kaparulinti@mail.ru>
2025-10-11 02:34:47 +0300

    adding config for ws

---
+++ src/main/java/org/itmo/isLab1/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package org.itmo.isLab1.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.itmo.isLab1.common.ws.WebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler<Object> webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+        .addHandler(webSocketHandler, "/ws")
+        .setAllowedOrigins("*", "http://localhost:3000", "http://localhost:5000");
+    }
+}
