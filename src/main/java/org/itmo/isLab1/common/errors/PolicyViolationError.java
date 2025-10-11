d06cc48eef6e6d3a472ed4ea13c720beeb45e13c
Luhter1 <kaparulinti@mail.ru>
2025-10-11 18:19:48 +0300

    updating errors

--- src/main/java/org/itmo/isLab1/common/errors/PolicyViolationError.java
+++ src/main/java/org/itmo/isLab1/common/errors/PolicyViolationError.java
@@ -1,5 +1,9 @@
 package org.itmo.isLab1.common.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class PolicyViolationError extends RuntimeException {
     public PolicyViolationError() {
         super();
