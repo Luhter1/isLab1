ec671e72c03598ddcb763e1b9c5d5cb1f8c59090
Luhter1 <kaparulinti@mail.ru>
2025-10-13 06:39:59 +0300

    add crudService interface

---
+++ frontend/src/interfaces/crud/CrudService.ts
@@ -0,0 +1,18 @@
+import { AxiosResponse } from 'axios';
+import Paged from '@/interfaces/models/Paged';
+
+export function staticImplements<T>() {
+  return <U extends T>(constructor: U) => {constructor;};
+}
+
+export default interface CrudService<Dto, CreateDto, UpdateDto> {
+  getAll(page: number, size: number, sort: string[]): Promise<AxiosResponse<Paged<Dto>>>;
+
+  get(id: number): Promise<AxiosResponse<Dto>>;
+
+  create(dto: CreateDto): Promise<AxiosResponse<Dto>>;
+
+  update(id: number, dto: UpdateDto): Promise<AxiosResponse<Dto>>;
+
+  delete(id: number): Promise<AxiosResponse>;
+}
