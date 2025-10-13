fc615ab2bc8ce134d10b31e6fafb3f48ac3f39df
Luhter1 <kaparulinti@mail.ru>
2025-10-13 06:54:54 +0300

    add all of Controllers

---
+++ frontend/src/controllers/LocationController.ts
@@ -0,0 +1,31 @@
+import api from '@/controllers/api';
+import { AxiosResponse } from 'axios';
+import { LocationDto } from '@/interfaces/dto/locations/LocationDto';
+import { LocationCreateDto } from '@/interfaces/dto/locations/LocationCreateDto';
+import { LocationUpdateDto } from '@/interfaces/dto/locations/LocationUpdateDto';
+import CrudService, { staticImplements } from '@/interfaces/crud/CrudService';
+import Paged from '@/interfaces/models/Paged';
+import { createCrudUri } from './utils/uri';
+
+@staticImplements<CrudService<LocationDto, LocationCreateDto, LocationUpdateDto>>()
+export default class LocationService {
+  static async getAll(page: number, size: number, sort: string[]): Promise<AxiosResponse<Paged<LocationDto>>> {
+    return api.get<Paged<LocationDto>>(`/locations${createCrudUri(page, size, sort)}`);
+  }
+
+  static async get(id: number): Promise<AxiosResponse<LocationDto>> {
+    return api.get<LocationDto>(`/locations/${id}`);
+  }
+
+  static async create(Location: LocationCreateDto): Promise<AxiosResponse<LocationDto>> {
+    return api.post<LocationDto>('/locations', Location);
+  }
+
+  static async update(id: number, Location: LocationUpdateDto): Promise<AxiosResponse<LocationDto>> {
+    return api.patch<LocationDto>(`/locations/${id}`, Location);
+  }
+
+  static async delete(id: number): Promise<AxiosResponse> {
+    return api.delete(`/locations/${id}`);
+  }
+}
---
+++ frontend/src/controllers/PersonController.ts
@@ -0,0 +1,31 @@
+import api from '@/controllers/api';
+import { AxiosResponse } from 'axios';
+import { PersonDto } from '@/interfaces/dto/people/PersonDto';
+import { PersonCreateDto } from '@/interfaces/dto/people/PersonCreateDto';
+import { PersonUpdateDto } from '@/interfaces/dto/people/PersonUpdateDto';
+import CrudService, { staticImplements } from '@/interfaces/crud/CrudService';
+import Paged from '@/interfaces/models/Paged';
+import { createCrudUri } from './utils/uri';
+
+@staticImplements<CrudService<PersonDto, PersonCreateDto, PersonUpdateDto>>()
+export default class PersonService {
+  static async getAll(page: number, size: number, sort: string[]): Promise<AxiosResponse<Paged<PersonDto>>> {
+    return api.get<Paged<PersonDto>>(`/people${createCrudUri(page, size, sort)}`);
+  }
+
+  static async get(id: number): Promise<AxiosResponse<PersonDto>> {
+    return api.get<PersonDto>(`/people/${id}`);
+  }
+
+  static async create(Person: PersonCreateDto): Promise<AxiosResponse<PersonDto>> {
+    return api.post<PersonDto>('/people', Person);
+  }
+
+  static async update(id: number, Person: PersonUpdateDto): Promise<AxiosResponse<PersonDto>> {
+    return api.patch<PersonDto>(`/people/${id}`, Person);
+  }
+
+  static async delete(id: number): Promise<AxiosResponse> {
+    return api.delete(`/people/${id}`);
+  }
+}
