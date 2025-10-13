3fc54b947aa514d81284deb183e2bc522d09b852
Luhter1 <kaparulinti@mail.ru>
2025-10-13 09:03:46 +0300

    add view for Location

---
+++ frontend/src/components/Location/Card.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { LocationDto } from '@/interfaces/dto/locations/LocationDto'
+
+interface Props {
+  obj: LocationDto
+}
+const props = defineProps<Props>()
+const data = props.obj
+</script>
+
+<template>
+  <el-card style="max-width: 480px">
+    <template #header>
+      <div class="card-header">
+        <span>Location</span>
+      </div>
+    </template>
+    <p :key="data.id" class="text item">{{ 'ID: ' + data.id }}</p>
+    <template #footer>
+        <p :key="data.x" class="text item">{{ 'X: ' + data.x }}</p>
+        <p :key="data.y" class="text item">{{ 'Y: ' + data.y }}</p>
+        <p :key="data.z" class="text item">{{ 'Z: ' + data.z }}</p>
+        <p :key="data.createdBy.username " class="text item">{{ 'Creator: ' + data.createdBy.username }}</p>
+    </template>
+  </el-card>
+</template>
--- frontend/src/components/Location/View.vue
+++ frontend/src/components/Location/View.vue
@@ -1,6 +1,64 @@
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import { getLocation } from '@/services/LocationService'
+import type { FormInstance, FormRules } from 'element-plus'
+import { LocationDto } from '@/interfaces/dto/locations/LocationDto'
+import cardLocation from './Card.vue'
+
+interface ViewId {
+  id: number;
+}
+const getData = ref<LocationDto>()
+
+const ruleFormRef = ref<FormInstance>()
+const view = reactive<ViewId>({
+  id: null
+});
+
+const validateId = (rule: any, value: number, callback: any) => {
+  if (!value) {
+    return callback(new Error('Input ID'))
+  }
+  if (value < 1) {
+    return callback(new Error('ID must be greater than 0'))
+  }
+  callback()
+}
+
+const rules = reactive<FormRules<ViewId>>({
+  id: [
+    { validator: validateId, trigger: 'blur' },
+  ]
+})
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate(async (valid) => {
+    if (valid) {
+      getData.value = await getLocation(view.id)
+    }
+  })
+}
+</script>
+
 <template>
-  <div>
-    <h1>Страница просмотра</h1>
-    <!-- Содержимое для создания -->
-  </div>
+  <el-form
+    ref="ruleFormRef"
+    style="max-width: 600px"
+    :model="view"
+    :rules="rules"
+    label-width="auto"
+  >
+
+    <el-form-item label="ID" prop="id">
+      <el-input v-model="view.id" type="number"/>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
+    </el-form-item>
+
+  </el-form>
+
+  <cardLocation :obj="getData" v-if="getData"/>
 </template>
