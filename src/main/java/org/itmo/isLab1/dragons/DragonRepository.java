bc330c3bb2450a5f1ccb9fe61b601174a3952f4e
Luhter1 <kaparulinti@mail.ru>
2025-11-24 00:21:36 +0300

    fix for dragon in deepest cave

--- src/main/java/org/itmo/isLab1/dragons/DragonRepository.java
+++ src/main/java/org/itmo/isLab1/dragons/DragonRepository.java
@@ -3,6 +3,7 @@ package org.itmo.isLab1.dragons;
 import org.springframework.data.jpa.repository.Query;
 import org.itmo.isLab1.common.framework.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DragonRepository extends CrudRepository<Dragon> {
@@ -11,6 +12,18 @@ public interface DragonRepository extends CrudRepository<Dragon> {
 
     Optional<Dragon> findTopAgeByAgeIsNotNullOrderByAgeDesc();
 
-    @Query("SELECT d FROM Dragon d JOIN d.cave dc WHERE dc.depth = (SELECT MAX(c.depth) FROM DragonCave c)")
-    Optional<Dragon> findDragonInDeepestCave();
+    @Query("""
+    SELECT d FROM Dragon d
+    JOIN d.cave c
+    WHERE c.depth = (SELECT MAX(c2.depth) FROM DragonCave c2)
+    ORDER BY d.id
+    """)
+    List<Dragon> findDragonsInDeepestCave();
+
+    default Optional<Dragon> findDragonInDeepestCave() {
+        return findDragonsInDeepestCave().stream().findFirst();
+    }
+
+//     @Query("SELECT d FROM Dragon d JOIN d.cave dc WHERE dc.depth = (SELECT MAX(c.depth) FROM DragonCave c)")
+//     Optional<Dragon> findDragonInDeepestCave();
 }
