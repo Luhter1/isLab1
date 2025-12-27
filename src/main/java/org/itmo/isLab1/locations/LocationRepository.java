9407d5513b28d22fe5b36cf7dc52301a6daaf57f
Luhter1 <kaparulinti@mail.ru>
2025-12-27 22:40:29 +0300

    add Location constrains

--- src/main/java/org/itmo/isLab1/locations/LocationRepository.java
+++ src/main/java/org/itmo/isLab1/locations/LocationRepository.java
@@ -3,4 +3,5 @@ package org.itmo.isLab1.locations;
 import org.itmo.isLab1.common.framework.CrudRepository;
 
 public interface LocationRepository extends CrudRepository<Location> {
+    boolean existsByXAndYAndZAndNameAndIdNot(Long x, Integer y, Double z, String name, Integer id);
 }
